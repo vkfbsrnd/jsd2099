158fec7e1f9459d77dba105e24b1446303acda21
vkfbsrnd <101619604+vkfbsrnd@users.noreply.github.com>
2022-03-15 15:08:59 +0900

    Update main.js

--- main.js
+++ main.js
@@ -1,2 +1,3 @@
-const myheading = document.querySelector('h1');
+const myHeading = document.querySelector('h1');
 myHeading.textConent = 'Imagine Dragons';
+document.body.innerHTML="<h2>æ»≥Á«œººø‰</h2>"
